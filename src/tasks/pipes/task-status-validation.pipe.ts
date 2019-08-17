0521dbb7a046e0304c80d98002a13e6503c94516
Thanh Nguyen <vnscriptkid@gmail.com>
2019-08-17 19:21:49 +0700

    Add Custom Validation Pipe for checking Task Status at Param Level

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,18 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly statusValues = Object.values(TaskStatus);
+
+  transform(value: string, metadata: ArgumentMetadata) {
+    value = value.toUpperCase();
+    if (!this.statusValues.includes(value)) {
+      throw new BadRequestException(`'${value}' is an invalid status`);
+    }
+    return value;
+  }
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -15,6 +15,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -51,7 +52,7 @@ export class TasksController {
   @Patch('/:id')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }

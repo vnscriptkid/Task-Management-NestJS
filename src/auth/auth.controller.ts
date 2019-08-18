cf1602edefadf2edb6368a0c7275dc13eabc1747
Thanh Nguyen <vnscriptkid@gmail.com>
2019-08-18 21:28:58 +0700

    Add some sync validation on auth credentials

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,4 @@
-import {
-  Controller,
-  Post,
-  Body,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -12,9 +6,10 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('/signup')
-  @UsePipes(ValidationPipe)
-  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  @Post('signup')
+  signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
 }
--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,10 +1,15 @@
-import { IsNotEmpty, Length } from 'class-validator';
+import { IsNotEmpty, Length, Matches } from 'class-validator';
 
 export class AuthCredentialsDto {
   @IsNotEmpty()
+  @Length(4, 20)
   username: string;
 
   @IsNotEmpty()
   @Length(6, 20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain at least 1 upper case letter, 1 lower case letter, 1 number or special character',
+  })
   password: string;
 }

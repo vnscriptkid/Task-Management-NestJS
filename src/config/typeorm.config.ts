fc271507cee9a2bdd637cb853049110425d9422d
Thanh Nguyen <vnscriptkid@gmail.com>
2019-08-18 19:06:54 +0700

    Remove secret information and remove console.log

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,7 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-console.log(process.env.NODE_ENV);
-
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.PG_HOST,
@@ -13,15 +11,3 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   synchronize: true,
   ssl: true,
 };
-
-// export const typeOrmConfig: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   host: 'ec2-75-101-128-10.compute-1.amazonaws.com',
-//   port: 5432,
-//   username: 'xqdyjeltkepcil',
-//   password: '5d95b3f00cd15039282be4fd7154d49ec1c36841d3d33d04c99eaa987c39b5e2',
-//   database: 'dckqa44jj9pp8e',
-//   entities: [__dirname + '/../**/*.entity.ts'],
-//   synchronize: true,
-//   ssl: true,
-// };
